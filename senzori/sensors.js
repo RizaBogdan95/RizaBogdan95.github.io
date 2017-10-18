edb234c4fc529101bae4982a2ae9e4588fdae564
RizaBogdan95 <32520000+RizaBogdan95@users.noreply.github.com>
2017-10-18 19:22:03 +0300

    senzori

--- senzori/sensors.js
+++ senzori/sensors.js
@@ -2,6 +2,9 @@ window.addEventListener('deviceorientation',ondeviceorientation);
 
 function ondeviceorientation(event)
 {
-	event.alpha;
-	
+	document.getElementById("id_alpha").innerHTMLevent = event.alpha;
+	document.getElementById("id_beta").innerHTMLevent = event.beta;
+	document.getElementById("id_gamma").innerHTMLevent = event.gamma;
+
 }
+//--------------
