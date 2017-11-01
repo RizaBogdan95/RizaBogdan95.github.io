43aa34308a7d9e4e2371384469fd968f8e4dd275
RizaBogdan95 <32520000+RizaBogdan95@users.noreply.github.com>
2017-11-01 19:43:04 +0200

    patrat rotit

--- senzori/sensors.js
+++ senzori/sensors.js
@@ -22,6 +22,7 @@ function deseneaza_cerc_svg(gamma,beta)
 	
 }
 
+
 function deseneaza_cerc_canvas(gamma,beta)
 {
 	var canvas = document.getElementById("id_canvas");
@@ -34,7 +35,7 @@ function deseneaza_cerc_canvas(gamma,beta)
 	context.clearRect(0,0,w,h);
 
 	var centru = {x : w / 2 , y : h / 2};
-	var raza =10;
+	var latura =10;
 	var max_deplasare_x= w/2 -raza;
 	var max_deplasare_y= h /2-raza;
 
@@ -43,6 +44,29 @@ function deseneaza_cerc_canvas(gamma,beta)
 //gamma == 90 -> w - raza  
 	context.arc(centru.x - gamma/ 90 * max_deplasare_x , centru.y + beta / 90 * max_deplasare_y,raza,0,2* Math.PI);
 	context.stroke();	
+function deseneaza_patrat_canvas(alpha,gamma,beta)
+{
+	var canvas = document.getElementById("id_canvas");
+	var context = canvas.getContext("2d");
+
+	context.beginPath();
+	var w = canvas.getAttribute("width");
+	var h = canvas.getAttribute("height");
+
+	context.clearRect(0,0,w,h);
+	context.resetTransform();
+
+	var centru = {x : w / 2 , y : h / 2};
+	var latura  =10;
+	var max_deplasare_x= w/2 -x-latura/2;
+	var max_deplasare_y= h /2-latura/2;
+
+//gamma == 0 -> centru.x
+//gamma == -90 -> 0+raza
+//gamma == 90 -> w - raza  
+	//context.arc(centru.x - gamma/ 90 * max_deplasare_x , centru.y + beta / 90 * max_deplasare_y,raza,0,2* Math.PI);
+	context.strokeRect(centru.x-latura/2,centru.y -latura/2, latura , latura )
+}	
 }
 function ondeviceorientation(event)
 {
