fc94e1f43725b305700d441c07cdc0ad0acdadef
RizaBogdan95 <32520000+RizaBogdan95@users.noreply.github.com>
2017-11-01 18:18:05 +0200

    desenare cerc in functie de senzor

--- senzori/sensors.js
+++ senzori/sensors.js
@@ -1,8 +1,28 @@
-document.getElementById("id_logic_level_version").innerHTML="Logic level version: 2017.10.25.3";
+document.getElementById("id_logic_level_version").innerHTML="Business level version: 2017.11.01.1";
 window.addEventListener('deviceorientation',ondeviceorientation);
 window.addEventListener('devicemotion',ondevicemotion);
 
 
+function deseneaza_cerc(gamma,beta)
+{
+	var canvas = document.getElementById("id_canvas");
+	var context = canvas.getContext("2d");
+
+	context.beginPath();
+	var w = canvas.getAttribute("width");
+	var h = canvas.getAttribute("height");
+
+	var centru = {x : w / 2 , y : h / 2};
+	var raza =10;
+	var max_deplasare_x= w/2 -raza;
+	var max_deplasare_y= h /2-raza;
+
+//gamma == 0 -> centru.x
+//gamma == -90 -> 0+raza
+//gamma == 90 -> w - raza  
+	context.arc(centru.x + gamma/ 90 * max_deplasare_x , centru.y + beta / 90 * max_deplasare_y,raza,0,2* Math.PI);
+	context.stroke();	
+}
 function ondeviceorientation(event)
 {
 	document.getElementById("id_alpha").innerHTML=Math.round (event.alpha * 10)/10;
@@ -12,9 +32,12 @@ function ondeviceorientation(event)
 }
 function ondevicemotion(event)
 {
+	
 	document.getElementById("id_acc").innerHTML ="Acc" + Math.round(event.acceleration.x * 10) / 10 + " " + Math.round(event.acceleration.y * 10) / 10+ " " + Math.round(event.acceleration.z * 10) / 10;
 	var ag = event.accelerationIncludingGravity;
 	var gamma = 	Math.atan(ag.x / ag.z) * 180 / Math.PI;
 	var beta = 	Math.atan(ag.y / ag.z) * 180 / Math.PI;
+	
 	document.getElementById("id_acc_g").innerHTML ="Acc_g = " + Math.round(ag.x * 10) / 10 + " " + Math.round(ag.y * 10) / 10 + " " + Math.round(ag.z * 10) / 10 + "Gamma = " + Math.round(gamma * 10) / 10 + " Beta = " + Math.round(beta * 10) / 10;		
+    deseneaza_cerc(gamma,beta)
 }
