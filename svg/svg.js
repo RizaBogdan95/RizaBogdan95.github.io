26316f0670e1cc81402f43a663dfa84e4c6d21f4
RizaBogdan95 <32520000+RizaBogdan95@users.noreply.github.com>
2017-11-01 18:54:49 +0200

    exemplu svg

---
+++ svg/svg.js
@@ -0,0 +1,6 @@
+document.getElementById("id_logic_level_version").innerHTML="Business level version: 2017.11.01.1";
+
+var cerc = document.getElementById("id_circle");
+cerc.setAttribute("cx",100);
+cerc.setAttribute("cx",100);
+cerc.setAttribute("r",20);
