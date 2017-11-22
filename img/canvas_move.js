1ab531b379fcd7a41f13bfde6e418ceb8e638d6a
RizaBogdan95 <32520000+RizaBogdan95@users.noreply.github.com>
2017-11-22 18:33:59 +0200

    sss

--- img/canvas_move.js
+++ img/canvas_move.js
@@ -15,9 +15,13 @@ img.onload = function (){
 	context.drawImage(img, top_x,top_y,img_width,img_height);
 	var rect_img=img.getBoundingClientRect();
 
+
 }
 canvas.addEventListener("touchmove",on_touch_move);
+canvas.addEventListener("touch_start", on_touch_start);
 
+var offset_inside_image_x;
+var offset_inside_image_y;
 
 //---------------------------------------------------
 
@@ -28,12 +32,25 @@ function on_touch_move(e)
 	var touches= e.changedTouches;
 	for (var i = 0; i < touches.length; i++) 
 		if (touches[i].pageX < top_x + img_width && touches[i].pageX >= top_x &&
-			touches[i].pageY < top_y + img_width && touches[i].pageY >= top_y) 
-			var offset_inside_image_x =(touches[i].pageX - rect_canvas.left) - top_x;
-			var offset_inside_image_y =(touches[i].pageY - rect_canvas.top)-top_y;
-			context.clearRect(0,0,400,300);
-				top_x = touches[i].pageX - rect_canvas.left-offset_inside_image_x;
-				top_y = touches[i].pageY - rect_canvas.top--offset_inside_image_y;
-				context.drawImage(img, top_x,top_y,img_width,img_height);
+			touches[i].pageY < top_y + img_width && touches[i].pageY >= top_y) {
+			 offset_inside_image_x =(touches[i].pageX - rect_canvas.left) - top_x;
+			 offset_inside_image_y =(touches[i].pageY - rect_canvas.top)-top_y;
+			}
+			
 	}
 }
+//------------------------------------------------------
+function on_touch_move(e)
+{
+e.preventDefault();
+
+	var touches= e.changedTouches;
+	for (var i = 0; i < touches.length; i++) 
+		if (touches[i].pageX < top_x + img_width && touches[i].pageX >= top_x &&
+			touches[i].pageY < top_y + img_width && touches[i].pageY >= top_y) {
+			 
+			context.clearRect(0,0,400,300);
+			top_x = touches[i].pageX - rect_canvas.left-offset_inside_image_x;
+			top_y = touches[i].pageY - rect_canvas.top--offset_inside_image_y;
+			context.drawImage(img, top_x,top_y,img_width,img_height);
+}				
